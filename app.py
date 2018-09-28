8031f6c62f3a079a668882c392ccaeadc225a28b
shiftp <shihpwu@gmail.com>
2018-09-28 16:58:57 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,58 @@
+#載入LineBot所需要的套件
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+
+app = Flask(__name__)
+
+#自己APP的token
+token=os.environ['MYTOKEN']
+secret = os.environ['MYSECRET']
+ID =os.environ['MYID']
+
+
+# 必須放上自己的Channel Access Token
+line_bot_api = LineBotApi(toke)
+# 必須放上自己的Channel Secret
+handler = WebhookHandler(secret)
+line_bot_api.push_message(ID, TextSendMessage(text='你可以開始了'))
+
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+#訊息傳遞區塊
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    print("Handle: reply_token: " + event.reply_token + ", message: " + event.message.text)
+    content = "{}: {}".format(event.source.user_id, event.message.text)
+    line_bot_api.reply_message(event.reply_token,message)
+
+#主程式
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
